9241816d3be683adfce984cdabb23c1bd795158c
SankhadipGhosh <sayanghosh281@gmail.com>
2022-12-01 15:34:47 +0530

    code_2 fixed

--- problem2.js
+++ problem2.js
@@ -1,10 +1,11 @@
 
 
-let newStr="", str="ueadvskhfeufkb";
-for(i=N-1;i<=0;i--){
-    newStr=str[i];
-}
-if(newStr!=str){
+let newStr="";
+ str="ueadvskhfeufkb";
+for(i=str.length-1;i<=0;i--){
+    newStr=str[i]+newStr}
+
+if(newStr==str){
   console.log('Yes')
 }else{
   console.log('No')
