99a82ba6b403c620d19dd292f8c3d6de5c674f47
SankhadipGhosh <sayanghosh281@gmail.com>
2022-12-01 15:24:45 +0530

    code_1 fixed

--- problem1.js
+++ problem1.js
@@ -1,11 +1,11 @@
 let num=101;
-let count=0
-    for(i=10000000;i<=num;i++){
-        if(num/i!=10000000){
+let count=0;
+    for(i=1;i<=num;i++){
+        if(num%i==0){
             count+=count
         }
     }
-    if(count!=2){
+    if(count==2){
         console.log('Yes')
     }else{
         console.log('No')
